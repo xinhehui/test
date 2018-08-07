eb89a25d367dc2ab383dada443411e4b0580efc0
许鑫 <xuxin@xinhehui.com>
2018-08-07 13:44:05 +0800

    perfomance

---
+++ packages/perfomance/PerformanceResourceTiming.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+
+
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://m.xinhehui.com', {waitUntil: 'networkidle2'});
+
+  const res = await page.evaluate(() => {
+    function assetParser(url) {
+      const { host, pathname } = new URL(url)
+    
+      const assetName = pathname.split('/').pop()
+    
+      return { assetType: assetName.includes('.') ? assetName.split('.').pop() : null, name: assetName || host, host }
+    }
+
+    // 获取页面加载的资源信息
+    return performance
+      .getEntriesByType('resource')
+      .map(({ name, initiatorType }) => {
+        return {
+          initiatorType,
+          ...assetParser(name)
+        }
+      })
+
+  })
+
+  console.log(res)
+
+  await browser.close();
+})();
+
+
+
+
