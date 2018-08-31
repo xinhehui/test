e9f19ef004022294bcddbab95e09c2fdc78d6fa0
许鑫 <xuxin@xinhehui.com>
2018-08-31 11:34:13 +0800

    color

---
+++ packages/html2canvas/src/parsing/border.js
@@ -0,0 +1,51 @@
+// flow
+'use strict'
+
+import Color from '../Color'
+
+export const BORDER_STYLE = {
+  NONE: 0,
+  SOLID: 1
+}
+
+export type BorderStyle = $Values<typeof BORDER_STYLE>
+
+export type Border = {
+  borderColor: Color,
+  BorderStyle: BorderStyle,
+  borderWidth: number
+}
+
+export const BORDER_SIDES = {
+  TOP: 0,
+  RIGHT: 1,
+  BOTTOM: 2,
+  LEFT: 3
+}
+
+// eslint-disable-next-line no-undef
+export type BorderSide = $Values<typeof BORDER_SIDES>
+
+const SIDES = Object.keys(BORDER_SIDES).map(s => s.toLowerCase())
+
+const parseBorderStyle = (style: string): BorderStyle => {
+  switch (style) {
+    case 'none':
+      return BORDER_STYLE.NONE
+  }
+  return BORDER_STYLE.SOLID
+}
+
+// eslint-disable-next-line no-undef
+export const parseBorder = (style: CSSStyleDeclaration): Array<Border> => {
+  return SIDES.map(side => {
+    const borderColor = new Color(style.getPropertyValue(`border-${side}-color`))
+    const borderStyle = parseBorderStyle(style.getPropertyValue(`border-${side}-style`))
+    const borderWidth = parseFloat(style.getPropertyValue(`border-${side}-width`))
+    return {
+      borderColor,
+      borderStyle,
+      borderWidth: isNaN(borderWidth) ? 0 : borderWidth
+    }
+  })
+}
